6b57747083cd9749bd1bee54284a34b4454fd74a
Daniel Pilla <daniel.b.pilla@gmail.com>
2020-12-17 15:30:46 -0500

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,32 @@
 from distutils.core import setup
+
+
+def readme():
+    """ print long description """
+    with open('README.md') as f:
+        long_descrip = f.read()
+    return long_descrip
+
+
 setup(
     name='qsaas',
     packages=['qsaas'],
     version='0.0.1',
     license='GPL-3.0',
     description='A wrapper for the Qlik Sense Enterprise SaaS APIs.',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
     author='Daniel Pilla',
     author_email='daniel.b.pilla@gmail.com',
     url='https://github.com/eapowertools/qsaas',
     download_url='https://github.com/eapowertools/qsaas/archive/v0.0.1-alpha.tar.gz',
-    keywords=['Qlik', 'API', 'REST', 'WRAPPER', 'QSAAS', 'QSESAAS'],
+    keywords=['Qlik', 'API', 'REST', 'Sense', 'QSAAS', 'QSESAAS'],
     install_requires=[
         'requests',
         'aiohttp',
         'asyncio',
     ],
+    python_requires='>=3.5',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
